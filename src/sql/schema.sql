0086d8ae323c2984065f726b60017507ccdfcf7d
Traore Bernard <bernard_traore@ens.univ-artois.fr>
2024-05-01 22:20:01 +0200

    Alter tables to include forgot field 'password'

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE administratifs (
     id_administratif INTEGER PRIMARY KEY AUTOINCREMENT,
     nom VARCHAR(30) NOT NULL,
-    createdAt DATE NOT NULL
+    mot_de_passe VARCHAR(20) NOT NULL,
+    date_creation DATE NOT NULL
 );
 
 CREATE TABLE matieres (
@@ -12,7 +13,8 @@ CREATE TABLE matieres (
 CREATE TABLE enseignants (
     id_enseignant INTEGER PRIMARY KEY AUTOINCREMENT,
     nom VARCHAR(30) NOT NULL,
-    createdAt DATE NOT NULL
+    mot_de_passe VARCHAR(20) NOT NULL,
+    date_creation DATE NOT NULL
 );
 
 CREATE TABLE salles (
@@ -24,10 +26,10 @@ CREATE TABLE crenaux (
     id_matiere INTEGER,
     id_enseignant INTEGER,
     id_salle INTEGER,
-    createdAt Date NOT NULL,
     heure_debut TIME,
     heure_fin TIME,
     type VARCHAR(2) NOT NULL, -- CM | TD | TP
+    date_creation Date NOT NULL,
     FOREIGN KEY (id_matiere) REFERENCES matieres(id_matiere),
     FOREIGN KEY (id_enseignant) REFERENCES enseignants(id_enseignant),
     FOREIGN KEY (id_salle) REFERENCES salles(id_salle)
@@ -42,7 +44,7 @@ CREATE TABLE promotions (
     id_promotion INTEGER PRIMARY KEY AUTOINCREMENT,
     id_departement INTEGER,
     nom VARCHAR(50) NOT NULL,
-    createdAt DATE NOT NULL,
+    date_creation DATE NOT NULL,
     FOREIGN KEY (id_departement) REFERENCES departements(id_departement)
 );
 
@@ -50,6 +52,7 @@ CREATE TABLE etudiants (
     id_etudiant INTEGER PRIMARY KEY AUTOINCREMENT,
     id_promotion INTEGER,
     nom VARCHAR(30) NOT NULL,
-    createdAt DATE NOT NULL,
+    mot_de_passe VARCHAR(20) NOT NULL,
+    date_creation DATE NOT NULL,
     FOREIGN KEY (id_promotion) REFERENCES promotions(id_promotion)
 );
